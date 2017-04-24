b386e382d2936133780b8ae178e55aaf34c5115c
Hope McIntyre <hope.e.mcintyre@gmail.com>
2017-04-23 20:01:44 -0400

    updated to oauth with token refresh

--- main.py
+++ main.py
@@ -4,10 +4,12 @@
 
 import spotipy
 import spotipy.util as util
+from spotipy import oauth2
 import os
 import sys
 import json
 import datetime
+import time
 
 # Replace Spotify Class with Class that includes Recent History
 from spotipy_mod import Spotify_mod
@@ -36,44 +38,56 @@ else:
     sys.exit()
 
 token = util.prompt_for_user_token(username, scope)
+sp_oauth = oauth2.SpotifyOAuth(key['ClientID'], key['ClientSecret'], 'http://localhost/', \
+scope=scope, cache_path=".cache-" + username )
 
-if token:
-    # Read the last endtime from tracker
-    with open('cursor.txt','r') as f:
-        start = f.read()
-    # print(start)
+for t in list(range(40)):
 
-    sp = spotipy.Spotify(auth=token)
+    if token:
 
-    # Warning: Submitting 50+ will NOT return a 'cursors' object
-    results = sp.current_user_recently_played(limit=49, after=start)
+        print(token)
+        token_info = sp_oauth.get_cached_token()
+        token = token_info['access_token']
+        # print(token_info['access_token'])
 
-    # Catch for no new songs in timewindow
-    if len(results['items']) == 0:
-        print('no new songs')
+        # Read the last endtime from tracker
+        with open('cursor.txt','r') as f:
+            start = f.read()
+        # print(start)
+
+        sp = spotipy.Spotify(auth=token)
+
+        # Warning: Submitting 50+ will NOT return a 'cursors' object
+        results = sp.current_user_recently_played(limit=49, after=start)
+
+        # Catch for no new songs in timewindow
+        if len(results['items']) == 0:
+            print('no new songs')
+        else:
+            # Write next start time to tracker
+            # print(results['cursors']['after'])
+            with open('cursor.txt','w') as f:
+                f.write(results['cursors']['after'])
+
+            # Store History in Txt File
+            filename = './history/HM_spotify_'+str(start)
+            print(filename)
+            with open(filename,'w') as f:
+                json.dump(results, f)
+
+            print(str(len(results['items']))+' new songs saved')
+
+            # Print History
+            # for item in results['items']:
+            #     print(item['played_at'])
+            #     track = item['track']
+            #     print(track['name'] + ' - ' + track['artists'][0]['name'])
+
+            # print(results['cursors'])
     else:
-        # Write next start time to tracker
-        # print(results['cursors']['after'])
-        with open('cursor.txt','w') as f:
-            f.write(results['cursors']['after'])
-
-        # Store History in Txt File
-        filename = './history/HM_spotify_'+str(start)
-        print(filename)
-        with open(filename,'w') as f:
-            json.dump(results, f)
-
-        print(str(len(results['items']))+' new songs saved')
-
-        # Print History
-        # for item in results['items']:
-        #     print(item['played_at'])
-        #     track = item['track']
-        #     print(track['name'] + ' - ' + track['artists'][0]['name'])
-
-        # print(results['cursors'])
-else:
-    print("Can't get token for", username)
+        print("Can't get token for", username)
+    print(str(t*20)+'minutes running')
+    time.sleep(20*60)
 
 # Write time/date details to log file
 with open('log.txt','a') as f:
